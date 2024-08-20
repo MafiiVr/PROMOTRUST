30ee022c271abd40ad3d43cb495d10b35a9b1a76
unknown <MafiiVr>
2024-08-20 13:31:56 -0500

    first commit

---
+++ src/main/java/pe/edu/upc/promotrust/PromotrustApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.promotrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotrustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotrustApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/promotrust/PromotrustApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.promotrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotrustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
