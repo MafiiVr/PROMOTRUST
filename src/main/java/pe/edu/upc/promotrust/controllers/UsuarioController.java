8386003745f738ae9218ae3a0591d73226031446
delilux <142470442+delilux@users.noreply.github.com>
2024-08-28 08:57:05 -0500

    Creando el primer metodo en Controller
    Se realizó un post para cumplir con la US30. Se pueden crear nuevos usuarios.

--- src/main/java/pe/edu/upc/promotrust/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/promotrust/controllers/UsuarioController.java
@@ -1,4 +1,29 @@
 package pe.edu.upc.promotrust.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.promotrust.dtos.UsuarioDTO;
+import pe.edu.upc.promotrust.entities.Usuario;
+import pe.edu.upc.promotrust.serviceinterface.IUsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @Autowired
+    private IUsuarioService uS;
+
+    @PostMapping
+    public void registrar (@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario u= m.map(dto, Usuario.class); //transformamos el dto
+        uS.insert(u);
+    };
+
+
+
 }
