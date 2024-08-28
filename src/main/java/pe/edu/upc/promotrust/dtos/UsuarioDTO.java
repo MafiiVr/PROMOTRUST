ecbe1b3b2ad26a3eb35c2436bb93b5d30a8a4b39
delilux <142470442+delilux@users.noreply.github.com>
2024-08-28 07:36:30 -0500

    Contenido usuario dto
    creacion de setters and getters

--- src/main/java/pe/edu/upc/promotrust/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/promotrust/dtos/UsuarioDTO.java
@@ -1,4 +1,75 @@
 package pe.edu.upc.promotrust.dtos;
 
 public class UsuarioDTO {
+
+    private int idusuario;
+
+    private String nombre;
+
+    private String contrasenia;
+
+    private String apellidos;
+
+    private int telefono;
+
+    private String correo;
+
+    private int RUC;
+
+    //Setters and getters
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getRUC() {
+        return RUC;
+    }
+
+    public void setRUC(int RUC) {
+        this.RUC = RUC;
+    }
 }
