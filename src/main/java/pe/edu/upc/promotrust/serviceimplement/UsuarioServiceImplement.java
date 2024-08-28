014e6741dbff6352fdc0666d4f2dbf4130668975
delilux <142470442+delilux@users.noreply.github.com>
2024-08-28 08:40:27 -0500

    Corregir serviceimplement

--- src/main/java/pe/edu/upc/promotrust/serviceimplement/UsuarioServiceImplement.java
+++ src/main/java/pe/edu/upc/promotrust/serviceimplement/UsuarioServiceImplement.java
@@ -3,16 +3,17 @@ package pe.edu.upc.promotrust.serviceimplement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.promotrust.entities.Usuario;
+import pe.edu.upc.promotrust.repositories.IUsuarioRepository;
 import pe.edu.upc.promotrust.serviceinterface.IUsuarioService;
 
 @Service
 public class UsuarioServiceImplement implements IUsuarioService {
 
     @Autowired
-    private IUsuarioService uS;
+    private IUsuarioRepository uS;
 
     @Override
     public void insert(Usuario usuario) {
-        
+        uS.save(usuario);
     }
 }
