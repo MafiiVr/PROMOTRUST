bdbbaebad7acc7835f7d81b07ebacd2ec2575906
delilux <142470442+delilux@users.noreply.github.com>
2024-09-02 22:27:40 -0500

    Update MetricasDTO.java

--- src/main/java/pe/edu/upc/promotrust/dtos/MetricasDTO.java
+++ src/main/java/pe/edu/upc/promotrust/dtos/MetricasDTO.java
@@ -1,4 +1,64 @@
 package pe.edu.upc.promotrust.dtos;
 
 public class MetricasDTO {
+
+    private int idMetrica;
+
+    public int alcancepersonas;
+
+    public double ingresosgenerados;
+
+    public int likes;
+
+    public String comentarios;
+
+    public int compartidos;
+
+    public int getIdMetrica() {
+        return idMetrica;
+    }
+
+    public void setIdMetrica(int idMetrica) {
+        this.idMetrica = idMetrica;
+    }
+
+    public int getAlcancepersonas() {
+        return alcancepersonas;
+    }
+
+    public void setAlcancepersonas(int alcancepersonas) {
+        this.alcancepersonas = alcancepersonas;
+    }
+
+    public double getIngresosgenerados() {
+        return ingresosgenerados;
+    }
+
+    public void setIngresosgenerados(double ingresosgenerados) {
+        this.ingresosgenerados = ingresosgenerados;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public int getCompartidos() {
+        return compartidos;
+    }
+
+    public void setCompartidos(int compartidos) {
+        this.compartidos = compartidos;
+    }
 }
