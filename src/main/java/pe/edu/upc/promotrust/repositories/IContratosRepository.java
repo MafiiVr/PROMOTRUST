e76b7ec075b9d3801ed45cf3a0a4bbce724dbe64
delilux <142470442+delilux@users.noreply.github.com>
2024-09-02 22:35:15 -0500

    JpaRepository para ContratoRepository
    Actualizado

--- src/main/java/pe/edu/upc/promotrust/repositories/IContratosRepository.java
+++ src/main/java/pe/edu/upc/promotrust/repositories/IContratosRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.promotrust.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pe.edu.upc.promotrust.entities.Contrato;
 
 @Repository
-public interface IContratosRepository {
+public interface IContratosRepository extends JpaRepository<Contrato, Integer> {
 }
