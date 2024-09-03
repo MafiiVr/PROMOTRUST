c8dc4a67c49f796fa5cf2d5ccd2588926fbfea11
delilux <142470442+delilux@users.noreply.github.com>
2024-09-02 22:50:48 -0500

    Create MetricasController.java
    1 get, 1 post y 1 delete

---
+++ src/main/java/pe/edu/upc/promotrust/controllers/MetricasController.java
@@ -0,0 +1,39 @@
+package pe.edu.upc.promotrust.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.promotrust.dtos.MetricasDTO;
+import pe.edu.upc.promotrust.entities.Metricas;
+import pe.edu.upc.promotrust.serviceinterface.IMetricasService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/metricas")
+public class MetricasController {
+
+    @Autowired
+    private IMetricasService mS;
+
+    @GetMapping
+    public List<MetricasDTO> listar(){
+        return mS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x, MetricasDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void registrar(@RequestBody  MetricasDTO dto){
+        ModelMapper m=new ModelMapper();
+        Metricas met=m.map(dto, Metricas.class);
+        mS.insert(met);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id) {
+        mS.delete(id);
+    }
+}
