5cd74b118ea8358443a26808f07309a7c5fbab96
delilux <142470442+delilux@users.noreply.github.com>
2024-09-02 22:27:43 -0500

    Update MetricasServiceImplement.java

--- src/main/java/pe/edu/upc/promotrust/serviceimplement/MetricasServiceImplement.java
+++ src/main/java/pe/edu/upc/promotrust/serviceimplement/MetricasServiceImplement.java
@@ -1,4 +1,34 @@
 package pe.edu.upc.promotrust.serviceimplement;
 
-public class MetricasServiceImplement {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.promotrust.entities.Metricas;
+import pe.edu.upc.promotrust.repositories.IMetricasRepository;
+import pe.edu.upc.promotrust.serviceinterface.IMetricasService;
+
+import java.util.List;
+
+@Service
+public class MetricasServiceImplement implements IMetricasService {
+
+    @Autowired
+    private IMetricasRepository mR;
+
+    @Override
+    public List<Metricas> list() {
+        return mR.findAll();
+    }
+
+    @Override
+    public void insert(Metricas metricas) {
+        mR.save(metricas);
+
+    }
+
+    @Override
+    public void delete(int id) {
+        mR.deleteById(id);
+
+    }
+
 }
