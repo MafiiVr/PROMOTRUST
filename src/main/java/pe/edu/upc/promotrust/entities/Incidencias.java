af37a67a2300cd2ad7e7d5f288f8610d6475f9e5
MafiiVr <vallejosr.mafer@gmail.com>
2024-09-03 10:12:24 -0500

    Indicendias "modificacion"

--- src/main/java/pe/edu/upc/promotrust/entities/Incidencias.java
+++ src/main/java/pe/edu/upc/promotrust/entities/Incidencias.java
@@ -3,7 +3,6 @@ package pe.edu.upc.promotrust.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "Incidencias")
+
 public class Incidencias {
 }
