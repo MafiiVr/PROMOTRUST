ce62e2f675d4eba149d05ec965220d5adfccd555
delilux <142470442+delilux@users.noreply.github.com>
2024-09-02 22:27:49 -0500

    Update IMetricasService.java

--- src/main/java/pe/edu/upc/promotrust/serviceinterface/IMetricasService.java
+++ src/main/java/pe/edu/upc/promotrust/serviceinterface/IMetricasService.java
@@ -1,4 +1,15 @@
 package pe.edu.upc.promotrust.serviceinterface;
 
+import pe.edu.upc.promotrust.entities.Metricas;
+
+import java.util.List;
+
 public interface IMetricasService {
+
+    public List<Metricas> list();
+
+    public void insert(Metricas metricas);
+
+    public void delete(int id);
+
 }
