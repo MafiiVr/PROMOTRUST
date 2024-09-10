d17c5ba78a02d8d0d293d3d9b7411c55c7f046cc
MafiiVr <vallejosr.mafer@gmail.com>
2024-09-10 00:29:55 -0500

    ContraUsuario

--- src/main/java/pe/edu/upc/promotrust/entities/ContratoUsuario.java
+++ src/main/java/pe/edu/upc/promotrust/entities/ContratoUsuario.java
@@ -1,4 +1,82 @@
 package pe.edu.upc.promotrust.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "ContratoUsuario")
 public class ContratoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDate fecha_inicio;
+
+    @Column(name = "fecha_fin", nullable = false)
+    private LocalDate fecha_fin;
+
+    @ManyToOne
+    @JoinColumn(name = "idContrato")
+    private Contrato contrato;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public ContratoUsuario() {}
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(LocalDate fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public LocalDate getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(LocalDate fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public Contrato getContrato() {
+        return contrato;
+    }
+
+    public void setContrato(Contrato contrato) {
+        this.contrato = contrato;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public ContratoUsuario(int id, LocalDate fecha_inicio, LocalDate fecha_fin, Contrato contrato, Usuario usuario) {
+        this.id = id;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.contrato = contrato;
+        this.usuario = usuario;
+    }
 }
