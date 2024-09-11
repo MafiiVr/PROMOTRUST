5a40dfa1daa2a8c14e0deabfa454eeb0cf5eeb7f
delilux <142470442+delilux@users.noreply.github.com>
2024-09-11 17:51:01 -0500

    ContratoUsuarioController

---
+++ src/main/java/pe/edu/upc/promotrust/controllers/ContratoUsuarioController.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.promotrust.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.promotrust.dtos.ContratoUsuarioDTO;
+import pe.edu.upc.promotrust.entities.ContratoUsuario;
+import pe.edu.upc.promotrust.serviceinterface.IContratoUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/contratousuario")
+public class ContratoUsuarioController {
+
+    @Autowired
+    private IContratoUsuarioService cCUS;
+
+    @GetMapping
+    public List<ContratoUsuarioDTO> listar(){
+        return cCUS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x, ContratoUsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void registrar(@RequestBody ContratoUsuarioDTO dto){
+        ModelMapper m=new ModelMapper();
+        ContratoUsuario ctu=m.map(dto, ContratoUsuario.class);
+        cCUS.insert(ctu);
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody ContratoUsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        ContratoUsuario ctu= m.map(dto, ContratoUsuario.class);
+        cCUS.update(ctu);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Integer id) {
+        cCUS.delete(id);
+    }
+}
