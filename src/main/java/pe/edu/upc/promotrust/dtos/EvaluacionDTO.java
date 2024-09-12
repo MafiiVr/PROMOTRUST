6fb7d55a399d09c75d92f3bda188b8c543b889d8
Ariana del Carpio <u202211917@upc.edu.pe>
2024-09-12 00:22:45 -0500

    Creacion del dto de evaluacion

--- src/main/java/pe/edu/upc/promotrust/dtos/EvaluacionDTO.java
+++ src/main/java/pe/edu/upc/promotrust/dtos/EvaluacionDTO.java
@@ -1,4 +1,54 @@
 package pe.edu.upc.promotrust.dtos;
 
+import jakarta.persistence.*;
+import pe.edu.upc.promotrust.entities.ContratoUsuario;
+
+import java.time.LocalDate;
+
 public class EvaluacionDTO {
+    private int idEvaluacion;
+    private Integer calificacion;
+    private String comentarios;
+    public LocalDate fecha_evaluacion;
+    private ContratoUsuario contratoUsuario;
+
+    public int getIdEvaluacion() {
+        return idEvaluacion;
+    }
+
+    public void setIdEvaluacion(int idEvaluacion) {
+        this.idEvaluacion = idEvaluacion;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public LocalDate getFecha_evaluacion() {
+        return fecha_evaluacion;
+    }
+
+    public void setFecha_evaluacion(LocalDate fecha_evaluacion) {
+        this.fecha_evaluacion = fecha_evaluacion;
+    }
+
+    public ContratoUsuario getContratoUsuario() {
+        return contratoUsuario;
+    }
+
+    public void setContratoUsuario(ContratoUsuario contratoUsuario) {
+        this.contratoUsuario = contratoUsuario;
+    }
 }
