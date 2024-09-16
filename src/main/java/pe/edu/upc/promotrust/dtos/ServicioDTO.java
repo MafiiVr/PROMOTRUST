ca6a13a0a1d7016d01f55036476cd560c341b0ea
MafiiVr <vallejosr.mafer@gmail.com>
2024-09-15 21:19:52 -0500

    Creacion del ServiceDTO

---
+++ src/main/java/pe/edu/upc/promotrust/dtos/ServicioDTO.java
@@ -0,0 +1,65 @@
+package pe.edu.upc.promotrust.dtos;
+
+import jakarta.persistence.Column;
+
+public class ServicioDTO {
+    private int idservicio;
+
+    private String nombre_servicio;
+
+    private String descripcion;
+
+    private Double precio;
+
+    private String categoria_servic;
+
+    private String estado_servic;
+
+    public int getIdservicio() {
+        return idservicio;
+    }
+
+    public void setIdservicio(int idservicio) {
+        this.idservicio = idservicio;
+    }
+
+    public String getNombre_servicio() {
+        return nombre_servicio;
+    }
+
+    public void setNombre_servicio(String nombre_servicio) {
+        this.nombre_servicio = nombre_servicio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria_servic() {
+        return categoria_servic;
+    }
+
+    public void setCategoria_servic(String categoria_servic) {
+        this.categoria_servic = categoria_servic;
+    }
+
+    public String getEstado_servic() {
+        return estado_servic;
+    }
+
+    public void setEstado_servic(String estado_servic) {
+        this.estado_servic = estado_servic;
+    }
+}
