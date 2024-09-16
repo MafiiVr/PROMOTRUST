c993917e23aeaccbb87f2748498ea3dbac94c07a
delilux <142470442+delilux@users.noreply.github.com>
2024-09-15 19:49:19 -0500

    Create CantidadMetricasEstadoContratoDTO.java
    Nuevo DTO para manejar el flujo de datos segun la query

---
+++ src/main/java/pe/edu/upc/promotrust/dtos/CantidadMetricasEstadoContratoDTO.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.promotrust.dtos;
+
+public class CantidadMetricasEstadoContratoDTO {
+
+    public String estadocontrato;
+
+    public int cantidadmetricas;
+
+    public String getEstadocontrato() {
+        return estadocontrato;
+    }
+
+    public void setEstadocontrato(String estadocontrato) {
+        this.estadocontrato = estadocontrato;
+    }
+
+    public int getCantidadmetricas() {
+        return cantidadmetricas;
+    }
+
+    public void setCantidadmetricas(int cantidadmetricas) {
+        this.cantidadmetricas = cantidadmetricas;
+    }
+}
