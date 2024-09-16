798c609e40408636d4e208aeb64d9607835233a9
delilux <142470442+delilux@users.noreply.github.com>
2024-09-15 19:25:50 -0500

    Update IMetricasRepository.java
    Se implementó query y totalingesos()

--- src/main/java/pe/edu/upc/promotrust/repositories/IMetricasRepository.java
+++ src/main/java/pe/edu/upc/promotrust/repositories/IMetricasRepository.java
@@ -1,9 +1,19 @@
 package pe.edu.upc.promotrust.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.promotrust.entities.Metricas;
 
+import java.util.List;
+
 @Repository
 public interface IMetricasRepository extends JpaRepository<Metricas, Integer> {
+
+    @Query(value = " SELECT c.id_contrato, SUM(m.ingresosgenerados)\n" +
+            " FROM contrato c\n" +
+            " JOIN metricas m ON c.id_contrato = m.contratoid\n" +
+            " GROUP BY c.id_contrato\n" +
+            " ORDER BY SUM(m.ingresosgenerados) DESC;", nativeQuery = true) //siempre poner el nativeQuery = true
+    public List<String []> totalingresos();
 }
