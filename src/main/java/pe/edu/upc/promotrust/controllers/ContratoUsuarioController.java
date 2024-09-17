c0ffee9f4634378cda7832bb773e6f22e3232b84
MafiiVr <vallejosr.mafer@gmail.com>
2024-09-17 01:40:54 -0500

    ContratoUsuario-Admin

--- src/main/java/pe/edu/upc/promotrust/controllers/ContratoUsuarioController.java
+++ src/main/java/pe/edu/upc/promotrust/controllers/ContratoUsuarioController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.promotrust.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.promotrust.dtos.ContratoUsuarioDTO;
 import pe.edu.upc.promotrust.entities.ContratoUsuario;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/contratousuario")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class ContratoUsuarioController {
 
     @Autowired
