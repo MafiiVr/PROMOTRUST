48b58f8e1e2ea387252a341de1f1178acb4d456b
MafiiVr <vallejosr.mafer@gmail.com>
2024-09-18 23:58:54 -0500

    Queries - Tips- Adicionales

--- src/main/java/pe/edu/upc/promotrust/controllers/TipsController.java
+++ src/main/java/pe/edu/upc/promotrust/controllers/TipsController.java
@@ -4,10 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.promotrust.dtos.CateogriaServiciosUsuriosDTO;
 import pe.edu.upc.promotrust.dtos.TipsDTO;
+import pe.edu.upc.promotrust.dtos.TipsPorUsuarioDTO;
 import pe.edu.upc.promotrust.entities.Tips;
 import pe.edu.upc.promotrust.serviceinterface.ITipsService;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,8 +57,21 @@ public class TipsController {
         return tp;
     }
 
-    @GetMapping("/listartipsporusuario")
-    public List<String[]> listartipsporusuario(){
-        return tS.listartipsporusuario();
+
+    @GetMapping("/tips_usuario")
+    public List<TipsPorUsuarioDTO> tipporusuario(){
+        List<String[]> filaLista= tS.tipporusuario();
+        List<TipsPorUsuarioDTO> dtoLista = new ArrayList<>();
+        for(String[] columna:filaLista){
+            TipsPorUsuarioDTO dto = new TipsPorUsuarioDTO();
+            dto.setUsuario_id(Integer.parseInt(columna[0]));
+            dto.setNombre_usuario(columna[1]);
+            dto.setApellido_usuario(columna[2]);
+            dto.setTip_id(Integer.parseInt(columna[3]));
+            dto.setContenido_tip(columna[4]);
+            dto.setFecha_creacion_tip(LocalDate.parse(columna[5]));
+            dtoLista.add(dto);
+        }
+        return dtoLista;
     }
 }
